3c2fc13296e4fe8dffc180dcccece971f8a99a5d
Papi-Eruh <alexis.deslandes@erudit.app>
2026-01-01 17:34:16 +0100

    Add export heart & maestro

--- lib/anecdotes.dart
+++ lib/anecdotes.dart
@@ -1,6 +1,9 @@
 /// A Very Good Project created by Very Good CLI.
 library;
 
+export 'package:heart/heart.dart' hide Anecdote;
+export 'package:maestro/maestro.dart';
+
 export 'src/models/models.dart';
 export 'src/widgets/anecdote_carousel.dart';
 export 'src/widgets/anecdote_widget.dart';
