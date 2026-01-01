abe9455e95423053366f65c1ff6cd03144c7c5d2
Papi-Eruh <alexis.deslandes@erudit.app>
2026-01-01 20:44:02 +0100

    Fixes

--- lib/src/widgets/anecdote_widget_impl.dart
+++ lib/src/widgets/anecdote_widget_impl.dart
@@ -2,9 +2,7 @@ import 'dart:async';
 
 import 'package:anecdotes/anecdotes.dart';
 import 'package:flutter/material.dart';
-import 'package:heart/heart.dart';
 import 'package:heart_flutter/heart_flutter.dart';
-import 'package:maestro/maestro.dart';
 import 'package:rxdart/rxdart.dart';
 
 class MeasureDecoratorWidget extends StatefulWidget {
